64c86363b7c2fea87ccaa6e28cafcb2e4557b185
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-03-17 13:22:19 -0400

    Application database structure setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-games = Movie.create([
-                       {
-                         question: '',
-                         choice1: '',
-                         choice2: '',
-                         choice3: '',
-                         education: ''
-                       },
-                       {
-                         question: '',
-                         choice1: '',
-                         choice2: '',
-                         choice3: '',
-                         education: ''
-                       },
-                       {
-                         question: '',
-                         choice1: '',
-                         choice2: '',
-                         choice3: '',
-                         education: ''
-                       }
-                     ])
+games = Game.create([
+                      {
+                        question: 'Does the world produce enough food for its inhabitants.',
+                        choice1: 'Not sure, but their doesnt appear to be enough food.',
+                        choice2: 'No, I see pictures of poverty in other places.',
+                        choice3: 'Yes, I have plenty of food',
+                        education: 'The food production worldwide, produces enough calories per capita. However, this does not eliminate poverty in all the world.'
+                      },
+                      {
+                        question: 'What does the Clean Water Act of 1948 do?',
+                        choice1: 'Ensures clean abundant water',
+                        choice2: 'Protects wildlife in the US waters.',
+                        choice3: 'All of the above.',
+                        education: 'The current Clean Water Act in the U.S. objective to ensure clean, abundant water supplies, and help to protect fish and other organisms in U.S. waters.'
+                      }
+                    ])
